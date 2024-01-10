103549960940befdb191107d62561977c87c2e7c
Yubeen Lee <yublee@student.42london.com>
2024-01-10 19:17:52 +0000

    first_petit_step

---
+++ push_swap.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 16:32:49 by yublee            #+#    #+#             */
+/*   Updated: 2024/01/10 19:16:29 by yublee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+//check error if error gain
+	ft_input_check(argc, argv);
+//if no error creating stack a from input
+	ft_create_stack(argv);
+//scan the number
+//choose argorithm
+//if all sorted
+//print out the actions executed
+}
